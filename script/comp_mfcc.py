fd927869553b1ea8ca805464c64eab82e81fbda7
Shun Yamashita <shun40tut@gmail.com>
2018-10-08 21:40:45 +0900

    処理時間計測と結果表示部分を微修正

--- script/comp_mfcc.py
+++ script/comp_mfcc.py
@@ -3,9 +3,10 @@ import numpy as np
 import librosa
 import Audio
 
-# (1) wavファイル読み込み
-print("#1 Wav files read")
 start = time.time()
+
+# (1) wavファイル読み込み
+print("#1 [Wav files read]")
 
 path1 = "wav/1_Piano_C3_Dur4_1.wav"
 path2 = "wav/2_Piano_C3_Dur4_2.wav"
@@ -25,12 +26,8 @@ x3, fs3 = librosa.load(path3, fs3)
 x4, fs4 = librosa.load(path4, fs4)
 x5, fs5 = librosa.load(path5, fs5)
 
-end = time.time()
-print("#1 elapsed time:", round(end - start, 4))
-
 # (2) 特徴抽出
-print("#2 Feature extraction")
-start = time.time()
+print("#2 [Feature extraction]")
 
 mfcc1 = librosa.feature.mfcc(x1, fs1)
 mfcc2 = librosa.feature.mfcc(x2, fs2)
@@ -38,12 +35,8 @@ mfcc3 = librosa.feature.mfcc(x3, fs3)
 mfcc4 = librosa.feature.mfcc(x4, fs4)
 mfcc5 = librosa.feature.mfcc(x5, fs5)
 
-end = time.time()
-print("#2 elapsed time:", round(end - start, 4))
-
 # (3) 類似度計算
-print("#3 Evaluation")
-start = time.time()
+print("#3 [Evaluation]")
 
 ac1, wp1 = librosa.core.dtw(mfcc1, mfcc1)
 ac2, wp2 = librosa.core.dtw(mfcc1, mfcc2)
@@ -57,11 +50,12 @@ eval3 = 1 - (ac3[-1][-1] / np.array(ac3).max())
 eval4 = 1 - (ac4[-1][-1] / np.array(ac4).max())
 eval5 = 1 - (ac5[-1][-1] / np.array(ac5).max())
 
-print("1 <--> 1:", round(eval1, 4))
-print("1 <--> 2:", round(eval2, 4))
-print("1 <--> 3:", round(eval3, 4))
-print("1 <--> 4:", round(eval4, 4))
-print("1 <--> 5:", round(eval5, 4))
+print("    1 <--> 1:", round(eval1, 4))
+print("    1 <--> 2:", round(eval2, 4))
+print("    1 <--> 3:", round(eval3, 4))
+print("    1 <--> 4:", round(eval4, 4))
+print("    1 <--> 5:", round(eval5, 4))
 
 end = time.time()
-print("#3 elapsed time:", round(end - start, 4))
+print("")
+print("Eelapsed time:", str(round(end - start, 4)) + "[sec]")
